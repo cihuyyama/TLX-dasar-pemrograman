c606b576784a75f08f568396708eaf28b571cf90
cihuyyama <59244067+cihuyyama@users.noreply.github.com>
2020-10-19 15:50:10 +0700

    Add files via upload

---
+++ Transpose Matriks/transposematriks.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a, b, c, d, e, f, g, h, i;
+	cin>>a>>b>>c;
+	cin>>d>>e>>f;
+	cin>>g>>h>>i;
+	cout<<a<<" "<<d<<" "<<g<<endl<<b<<" "<<e<<" "<<h<<endl<<c<<" "<<f<<" "<<i<<endl;
+}

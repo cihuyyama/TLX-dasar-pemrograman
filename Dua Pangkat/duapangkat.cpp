d6c16dfe4a9e587bacfb21b8f70a928096d64083
cihuyyama <59244067+cihuyyama@users.noreply.github.com>
2020-10-19 15:22:04 +0700

    Add files via upload

---
+++ Dua Pangkat/duapangkat.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	long n, m;
+	cin >> n;
+	m=1;
+	while (m<n){
+		m = m*2;
+}
+		if (n!=m){
+			cout << "bukan" << endl;
+		}
+		else {
+			cout << "ya" << endl;
+		}
+	
+}

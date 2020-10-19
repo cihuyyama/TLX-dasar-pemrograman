edb7215227de2d7b7823dc7e5813fbb5986912ab
cihuyyama <59244067+cihuyyama@users.noreply.github.com>
2020-10-19 15:27:46 +0700

    Add files via upload

---
+++ Ganji Genap/ganjilgenap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	if(i>50){
+		cout << endl;
+	}
+	else if(i<1){
+		cout << endl;
+	}
+	else if (n%2==0){
+		cout << "genap" << endl;
+	}
+	else {
+		cout << "ganjil" << endl;
+	}
+}
